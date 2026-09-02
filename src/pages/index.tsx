5a519ed9ebe6a33aafc112cb6faae277d524d1af
Diaz Nugraha <diaznugraha00@gmail.com>
2026-09-02 09:50:44 +0700

    feat: implement lenis smooth scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import HomePage from "@/components/home";
+import { LenisProvider } from "@/providers";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <HomePage />;
+  return (
+    <LenisProvider>
+      <HomePage />
+    </LenisProvider>
+  );
 }
