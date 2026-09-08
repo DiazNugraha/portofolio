ad6c084cfa45bd65f2543126fa8bdf0faa17484d
Diaz Nugraha <diaznugraha00@gmail.com>
2026-09-08 22:25:21 +0700

    chore: expand gap

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -19,7 +19,7 @@ export default function MainLayout({ children }: PropsWithChildren) {
   return (
     <div className="w-full h-full flex justify-center relative">
       <div className="lg:w-[30%] h-full">
-        <div className="flex px-5 py-36 text-white flex-col items-center gap-y-16">
+        <div className="flex px-5 py-36 text-white flex-col items-center gap-y-20">
           {children}
           <Footer />
         </div>
