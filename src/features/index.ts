9e2e585dc580875d70c8deeca25b51d5c84150c6
Diaz Nugraha <diaznugraha00@gmail.com>
2026-09-20 23:26:56 +0700

    refactor: remove unused page

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,7 +1,6 @@
 export { default as Tool } from "./tool";
 export { default as About } from "./about";
 export { default as Home } from "./home";
-export { default as Portfolio } from "./portfolio";
 export { default as Technology } from "./technology";
 export { default as Contact } from "./contact";
 
--- src/pages/portfolio/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Portfolio } from "@/features";
-
-export default function PortfolioPage() {
-  return <Portfolio />;
-}
